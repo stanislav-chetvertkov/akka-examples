5dfded97fbc921cce6d4b44f24aed479c10ae2f0
stanislav <schetvertkov@twilio.com>
2017-04-18 09:30:54 +0300

    filters

--- src/main/scala/patterns/filters/Example.scala
+++ src/main/scala/patterns/filters/Example.scala
@@ -2,12 +2,17 @@ package patterns.filters
 
 import akka.actor.{Actor, ActorRef}
 
+import scala.reflect.ClassTag
+
 class Example extends App {
 
   case class MyMessage(number: Int)
 
   trait Filter[T] {
-    self: Actor =>
+    self: Actor => //todo explain
+
+    //todo explain erasure
+    implicit def ctg: ClassTag[T]
 
     def next: ActorRef
 
@@ -19,10 +24,12 @@ class Example extends App {
   }
 
   class FilterA(val next: ActorRef) extends Actor with Filter[MyMessage] {
+    implicit val ctg = ClassTag(classOf[FilterA])
     override def filter(input: MyMessage): Boolean = input.number > 100
   }
 
   class FilterB(val next: ActorRef) extends Actor with Filter[MyMessage] {
+    implicit val ctg = ClassTag(classOf[FilterB])
     override def filter(input: MyMessage): Boolean = input.number < 200
   }
 
